15478041dfb7ec1f45367a9cfa1312d6be000cf2
Kego-S <setlhatsoe7.co@gmail.com>
2023-10-10 20:52:18 +0200

    title

--- Educator/App.js
+++ Educator/App.js
@@ -3,3 +3,7 @@ function learnMore() {
     alert("Learn more button clicked!");
     // You can add more functionality as needed
 }
+function redirectToGradePage(grade) {
+      // Replace with the actual URLs for the grade pages
+      window.location.href = "URL_TO_GRADE_" + grade + "_PAGE";
+    }
