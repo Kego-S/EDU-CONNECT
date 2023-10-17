70ad91d945535916beb7ef1963d7e900e6b9b593
Kego-S <setlhatsoe7.co@gmail.com>
2023-10-17 21:02:08 +0200

    index

---
+++ About/App.js
@@ -0,0 +1,6 @@
+// This script will make the image fade in when the page loads
+window.addEventListener('load', function() {
+    var image = document.querySelector('img');
+    image.classList.add('fade-in');
+  });
+  
