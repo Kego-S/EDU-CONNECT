56e6f65f6c0f94617a64ebef9f1af9517dc2681e
lefavilakazi97 <lefa.vilakazi97@gmail.com>
2023-10-21 09:50:26 +0200

    deleted about

--- About/App.js
+++
@@ -1,6 +0,0 @@
-// This script will make the image fade in when the page loads
-window.addEventListener('load', function() {
-    var image = document.querySelector('img');
-    image.classList.add('fade-in');
-  });
-  
